8a80f01a6d7c5f33c99378a39cbca5ae85defb9e
932436978@qq.com <932436978@qq.com>
2023-10-17 14:29:51 +0800

    fix DataIOTools

--- GIS_App_Dev/DataIOTools.cs
+++ GIS_App_Dev/DataIOTools.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms.Design;
+using MapObjects;
 
 namespace GIS_APP_DEV
 {
-    internal static class DataIOTools
+    public static class DataIOTools
     {
-        
+        public static Layer ReadShp(string filepath)
+        {
+            OSGeo.GDAL.Gdal.SetConfigOption("GDAL_FILENAME_IS_UTF8","YES");
+            OSGeo.GDAL.Gdal.SetConfigOption("SHAPE_ENCODING", "");
+            OSGeo.GDAL.Gdal.AllRegister();
+            OSGeo.OGR.Ogr.RegisterAll();
+
+            OSGeo.OGR.Driver pDriver = OSGeo.OGR.Ogr.GetDriverByName("ESRI Shapefile");
+            OSGeo.OGR.DataSource pDataSource = pDriver.Open(filepath, 1);
+            OSGeo.OGR.Layer pLayer = pDataSource.GetLayerByName(System.IO.Path.GetFileNameWithoutExtension(filepath));
+            
+        }
+
     }
 }
